aad31640e78ff5f9808ebfd69bb0225a4439757c
ivanl <ivanlisi88@gmail.com>
2023-06-22 19:26:47 +0300

    Revert "add empty var"
    This reverts commit 394ea7ae26f160205d59a5453bebf2245896b817.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # f99d79b5ace041bcbf35623860302bb8
 from news_ import NewsFeed
-a = ""
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     news_feed = NewsFeed(interest='tesla', from_date="2023-06-20", to_date="2023-06-21", language="en")
